b66a1f912c4be9fd34a9238303b262f924f7a50a
sudo-immanuele <emmanuel@steama.co>
2025-05-04 16:33:29 +0100

    Update 01
    added MPLABX FILE

---
+++ Projects/IEDS_TEAM/HYBRID INVERTER REVAMP/Design/rev01/rev01_1/IEDS_dspic30F4012.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=IEDS_dspic30F4012.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/IEDS_dspic30F4012.X.production.hex
